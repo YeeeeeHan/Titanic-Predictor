7e341c28e56d9beb879a92c3f38ee42cec084491
Lim Yee Han <limyeehan@gmail.com>
2020-06-14 00:03:01 +0800

    Add code for deployment to Heroku

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-  res.json(customers);
-});
+  // Load index HTML file for any request
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
+// Listen on port - deploying on heroku
 const port = process.env.PORT || 8000;
-
-// app.listen(port, () => console.log(`Server started on port ${port}`));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Server started on port ${port}`));
 
 
